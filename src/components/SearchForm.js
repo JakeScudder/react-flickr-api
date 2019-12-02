fc629474323ac577a4a924dae005d8bc83f7bc5f
Jake M <scudsterr@gmail.com>
2019-12-02 13:42:50 -0500

    Update SearchForm.js
    Changed to functional Component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,25 +1,32 @@
-import React, { forwardRef } from 'react';
+import React, { useState } from 'react';
 import { withRouter } from "react-router";
-//import { Link } from 'react-router-dom';
-
-//Stateful
 
 const SearchForm = (props) => {
 
+	//Set State
+	const [search, setSearch] = useState("");
+
 	//Handles the form submission, passes info to handleSearch props
-	const handleSubmit = event => {
-		event.preventDefault();
-		let input = search.value;
-		console.log(search.value);
-		let searchUrl = `/search/${input}`;
+	const handleSubmit = e => {
+		e.preventDefault();
+		let searchUrl = `/search/${search}`;
 		props.history.push(searchUrl);
-		event.currentTarget.reset();
-		props.handleSearch(input);
+		e.currentTarget.reset();
+		props.handleSearch(search);
 		}
+	const handleChange = e => {
+		setSearch(e.target.value);
+	}
 
 	return (
 		<form onSubmit={handleSubmit} className="search-form">
-			<input type="search" name="search" placeholder="Search" ref={ (input) => search = input } required/>
+			<input 
+				type="search" 
+				name="search" 
+				placeholder="Search" 
+				value={search}
+				onChange={handleChange}
+				/>
 				<button type="submit" className="search-button">
 					<svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
 						<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
