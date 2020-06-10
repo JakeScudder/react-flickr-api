1213f86114e2db5962ae41abb3fc653ff9138377
Jake Scudder <scudsterr@gmail.com>
2020-06-10 17:20:33 -0400

    Browser Navigation

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import Photos from './Photos';
-import NotFound from './NotFound';
+import React, { Component } from "react";
+import Photos from "./Photos";
+import NotFound from "./NotFound";
 //Stateful
 
 class PhotoContainer extends Component {
   constructor(props) {
     super(props);
-    this.setState({
-      query: ""
-    })
+    this.state = {
+      query: "",
+    };
   }
 
   componentDidMount() {
@@ -18,35 +18,45 @@ class PhotoContainer extends Component {
     }
   }
 
+  // Handles Browser Navigation
+  componentDidUpdate() {
+    window.onpopstate = (e) => {
+      // Delay for match params to be current
+      setTimeout(() => {
+        let query = this.props.match.params.input;
+        if (query) {
+          this.props.handleSearch(query);
+        }
+      }, 500);
+    };
+  }
+
   render() {
     //If no results, render NotFound Component
     if (this.props.data.length === 0 && this.props.loading === false) {
       return (
         <div className="photo-container">
           <h2>Results</h2>
-            <NotFound />
+          <NotFound />
         </div>
-      )
+      );
     } else {
-
-    //Otherwise return Photo components
-    return (
-      <div className="photo-container">
-        <h2>Results</h2>
-            <ul>
-              { this.props.data.map( photo=> {
-              return <Photos key={photo.id} photo={photo}/>
-              })}
-            </ul>
-      </div>        
-    );
-    }  
+      //Otherwise return Photo components
+      return (
+        <div className="photo-container">
+          <h2>Results</h2>
+          <ul>
+            {this.props.data.map((photo) => {
+              return <Photos key={photo.id} photo={photo} />;
+            })}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
 export default PhotoContainer;
-
-
 
 //Debugging Simple Formatting
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,49 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { withRouter } from "react-router";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 const SearchForm = (props) => {
-	
+  //Set State
+  const [search, setSearch] = useState("");
 
-	//Set State
-	const [search, setSearch] = useState("");
+  //Handles the form submission, passes info to handleSearch props
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let searchUrl = `/search/${search}`;
+    props.history.push(searchUrl);
+    props.handleSearch(search);
+    Cookies.set("searchQuery", JSON.stringify(search));
+    setSearch("");
+  };
 
-	//Handles the form submission, passes info to handleSearch props
-	const handleSubmit = e => {
-		e.preventDefault();
-		let searchUrl = `/search/${search}`;
-		props.history.push(searchUrl);
-		props.handleSearch(search);
-		Cookies.set('searchQuery', JSON.stringify(search))
-		setSearch("");
-	}
-	
-	const handleChange = e => {
-		setSearch(e.target.value);
-	}
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-	return (
-		<form onSubmit={handleSubmit} className="search-form">
-			<div className="searchDiv">
-				<input 
-					type="search" 
-					name="search" 
-					placeholder="Search" 
-					value={search}
-					onChange={handleChange}
-				/>
-			</div>
-			<div className="searchDiv">
-				<button type="submit" className="search-button">
-					<svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
-						<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-						<path d="M0 0h24v24H0z" fill="none"/>
-					</svg>
-				</button>
-			</div>
-		</form>
-	)
+  return (
+    <form onSubmit={handleSubmit} className="search-form">
+      <div className="searchDiv">
+        <input
+          type="search"
+          name="search"
+          placeholder="Search"
+          value={search}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="searchDiv">
+        <button type="submit" className="search-button">
+          <svg
+            fill="#fff"
+            height="24"
+            viewBox="0 0 23 23"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
+        </button>
+      </div>
+    </form>
+  );
 };
-  
+
 const SearchFormWithRouter = withRouter(SearchForm);
 export default SearchFormWithRouter;
